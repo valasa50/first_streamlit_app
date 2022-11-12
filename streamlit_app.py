ecb5086f6cea08b0e0bb67f45681a1d4b4bcdaee
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-12 18:55:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorite')
-streamlit.text('\N{flexed omega3} Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.text('\N{avocado} Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 
