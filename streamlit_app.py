c653235b064104e124fd1ac2c804e42d5c5d7ad9
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-12 18:49:37 +0530

    Update streamlit_app.py
    typo change

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.header('Breakfast Favorite')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
 
 
 
