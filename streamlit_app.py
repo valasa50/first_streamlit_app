64890cb7731ffb85a66075c001f1fb89fe1db4fe
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-12 19:05:01 +0530

    Update streamlit_app.py
    Import Pandas and open CSV file

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,7 @@ streamlit.text('\N{flexed biceps} Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
