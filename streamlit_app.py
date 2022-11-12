cde680271cef4262b87ae39118c3b934aa7e07c5
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-12 18:48:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorite')
 streamlit.text('🍛 Omega 3 & Blueberry Oatmeal')
-streamlit.text('('Kale, Spinach & Rocket Smoothie')
-streamlit.text(' Hard-Boiled Free-Range Egg')
-streamlist.text('('Avocado Toast')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlist.text('Avocado Toast')
 
 
