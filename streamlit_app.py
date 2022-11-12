9dbe7c8bcc052f83b584a53e182d5a99ce9c3b7e
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-12 18:23:18 +0530

    Update streamlit_app.py
    deleted extra lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.text('this is just for check')
 
-streamlist.test('issue')
