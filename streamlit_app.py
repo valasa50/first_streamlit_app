ef191e140983ecc0a19481c78aecac8832e84e5a
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-12 18:51:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 
 
