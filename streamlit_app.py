10910a8e66b0aafe7c0f05c09268babbaaaa2622
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-17 18:12:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,14 +28,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 # Convert from json to normelize  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as table
 streamlit.dataframe(fruityvice_normalized)
 streamlit.header('Fruityvice Fruit Advice!') import requests
 
-fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/kiwi")
-streamlit.text(fruityvice_response.json())
+fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_repsonse.json())
 streamlit.dataframe(fruityvice_normalized)
