e0e64f23545aef4c5e19304b4d41671a1f1597b0
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-17 18:23:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,5 @@ fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_repsonse.json())
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/"+"apple")
